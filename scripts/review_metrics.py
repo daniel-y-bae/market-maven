6d4f70f8c5b79f83d5a59097d5d37fef6bb2c4ab
Keith Dowd <keith.dowd@gmail.com>
2020-11-21 16:52:28 -0500

    Removed scripts

--- scripts/review_metrics.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import numpy as np
-import pandas as pd
-
-np.set_printoptions(formatter={'all':lambda x: str(x)})
-pd.set_option('display.float_format', lambda x: '%.3f' % x)
-
-df = pd.read_csv('metrics_v.csv')
